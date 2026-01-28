92c78694bf84a2436a7c71f8cbf926b5fe56d524
Gustavo Santana Moura <smgustavo09@gmail.com>
2026-01-28 01:23:15 +0000

    atualizado

--- xadrez.c
+++ xadrez.c
@@ -2,16 +2,33 @@
 
     int main(){
 
-        int rainha, torre, bispo;
+    //Área para adicionar as variáveis
+
+        int rainha, torre = 1, bispo = 1;
         
+    //Área para estrutura de repetição 'For' para simular a movimentação da peça de xadrez - Rainha
+
         for (rainha = 1; rainha <= 8; rainha++)
         {
-            printf("Esquerda\n");
+            printf("RAINHA - Esquerda\n");
         }
+
+    //Área para estrutura de repetição 'While' para simular a movimentação da peça de xadrez - Torre
+
+        while (torre <= 5)
+        {
+            printf("TORRE - Direita\n");
+            torre++;
+        }
+
+    //Área para estrutura de repetição 'Do-while' para simular a movimentação da peça de xadrez - Bispo
+
+        do
+        {
+            printf("BISPO - Cima Direita\n");
+            bispo++;
+        } while (bispo <= 5);
         
         
-
-
-
         return 0;
     }
