791d7b28e8daf8fca6067960bc66648693051d43
Gustavo Santana Moura <smgustavo09@gmail.com>
2026-01-28 01:22:57 +0000

    atualizado

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # xadrezC
+
+Movimentando as Peças do xadrez
+
+
+Este desafio foca na movimentação das peças de xadrez usando estruturas de repetição em C. Você aplicará o que aprendeu sobre for, while e do-while para simular movimentos no tabuleiro.
---
+++ xadrez.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(){
+
+        int rainha, torre, bispo;
+        
+        for (rainha = 1; rainha <= 8; rainha++)
+        {
+            printf("Esquerda\n");
+        }
+        
+        
+
+
+
+        return 0;
+    }
